7b0afb84d98e0c135178172b557a95a4bd31a0b5
张三丰 <123456@qq.com>
2019-04-10 22:15:25 +0800

    composer require jaeger/querylist

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit383a03e7158f045b9fcdc53f4c93cfe4
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit383a03e7158f045b9fcdc53f4c93cfe4::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire383a03e7158f045b9fcdc53f4c93cfe4($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire383a03e7158f045b9fcdc53f4c93cfe4($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
